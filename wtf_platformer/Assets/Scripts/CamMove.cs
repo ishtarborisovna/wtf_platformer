c453b424d6c150ed9aeca268e46d3660299df8a0
voloshina_ee <e.voloshina@corpwebgames.com>
2019-12-11 02:04:23 +0300

    коллайдеры, Гарричка научился прыгать и бегать, создана точка респауна

---
+++ wtf_platformer/Assets/Scripts/CamMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public GameObject Harry;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (Harry.transform.position.x, 0.2f, -10f);
+    }
+}
