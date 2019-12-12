b2a642cbcbcd5377f690270279f9f0165e186c90
voloshina_ee <e.voloshina@corpwebgames.com>
2019-12-12 20:31:36 +0300

    правка скрипта Гарри, переименована сцена

--- wtf_platformer/Assets/Scripts/HarryControl.cs
+++ wtf_platformer/Assets/Scripts/HarryControl.cs
@@ -7,21 +7,34 @@ public class HarryControl : MonoBehaviour
     public float speed = 20f;
     private Rigidbody2D rb;
     private bool faceRight = true;
+
+    [SerializeField]
+    private int lives = 5;
+    [SerializeField]
+    private float jumpForse = 5.0f;
+
+    private bool isGrounded = false;
+    //private Animator animator;
+    //private SpriteRenderer sprite;
     
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(Vector2.up * 4000);
+
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
 
         if (moveX > 0 && !faceRight)
             flip();
@@ -29,9 +42,24 @@ public class HarryControl : MonoBehaviour
             flip();
     }
 
-    void flip ()
+    private void flip ()
     {
         faceRight = !faceRight;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        //это косяк
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.83f);
+
+        isGrounded = colliders.Length > 1;
+    }
+
+
 }
