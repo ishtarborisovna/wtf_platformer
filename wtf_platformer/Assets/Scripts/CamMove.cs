43ec364381891991e51306937206e9868f59e5e8
voloshina_ee <e.voloshina@corpwebgames.com>
2019-12-30 16:07:18 +0300

    нельзя уйти за камеру назад

--- wtf_platformer/Assets/Scripts/CamMove.cs
+++ wtf_platformer/Assets/Scripts/CamMove.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class CamMove : MonoBehaviour
 {
     public GameObject Harry;
+    public GameObject BackWall;
 
     // Update is called once per frame
     void Update()
     {
         if (Harry.transform.position.x + 5f > transform.position.x)
+        {
             transform.position = new Vector3(Harry.transform.position.x + 5f, 0.2f, -10f);
+            BackWall.transform.position = new Vector3(transform.position.x - 9.2f, 0, 0);
+        }
 
     }
 }
