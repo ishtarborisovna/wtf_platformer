55dfa50f5ffb9f5443439d4fd4eca4d373f0c6f8
voloshina_ee <e.voloshina@corpwebgames.com>
2020-01-24 21:29:36 +0300

    кнопка включения и выключения музыки

---
+++ wtf_platformer/Assets/Scripts/MusicSwitch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSwitch : MonoBehaviour
+{
+    public Sprite enableSprite;
+    public Sprite disableSprite;
+
+    bool audioEnabled = true;
+    public bool AudioEnabled { get { return audioEnabled; } set { SetAudio(value); } }
+
+    Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void SetAudio(bool enabled)
+    {
+        if (enabled)
+        {
+            AudioListener.volume = 1f;
+            image.sprite = enableSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+            image.sprite = disableSprite;
+        }
+        audioEnabled = enabled;
+    }
+
+    public void SwitchAudio()
+    {
+        AudioEnabled = !AudioEnabled;
+    }
+}
