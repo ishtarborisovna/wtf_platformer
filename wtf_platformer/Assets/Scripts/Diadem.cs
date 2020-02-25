c531ffc123dfcf5a985df0dd097e38ba492348b0
voloshina_ee <e.voloshina@corpwebgames.com>
2020-02-26 02:03:25 +0300

    встреча тома и гарри

---
+++ wtf_platformer/Assets/Scripts/Diadem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Diadem : MonoBehaviour
+{
+    [SerializeField]
+    PlayableDirector director;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            director.Play();
+            Destroy(gameObject);
+        }
+    }
+}
