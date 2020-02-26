4aeaad1699ea72bee71c56af9f90e84aeddb55ce
voloshina_ee <e.voloshina@corpwebgames.com>
2020-02-26 14:59:25 +0300

    вплоть до выбора в конце

---
+++ wtf_platformer/Assets/Scripts/Choice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choice : MonoBehaviour
+{
+    public GameObject Harry;
+    private bool choising = false;
+    public GameObject ToTom;
+    public GameObject ToFight;
+
+    private void Update()
+    {
+        if (!choising) Harry.GetComponent<HarryControl>().enabled = false;
+    }
+    public void Chois(int isChois)
+    {
+        if (isChois == 1) 
+        {
+            choising = true;
+            Harry.GetComponent<HarryControl>().enabled = true;
+            ToTom.SetActive(true);
+            Destroy(gameObject);
+        }
+        if (isChois == 2) 
+        {
+            choising = true;
+            Harry.GetComponent<HarryControl>().enabled = true;
+            ToFight.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
--- wtf_platformer/Assets/Scripts/Diadem.cs
+++ wtf_platformer/Assets/Scripts/Diadem.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class Diadem : MonoBehaviour
 {
     [SerializeField]
     PlayableDirector director;
+    public GameObject ToLvl6;
+
+    private void Update()
+    {
+        if (ToLvl6.activeInHierarchy)
+        {
+            GameManagerScript.points = HarryControl.ruby;
+            SceneManager.LoadScene("Lvl6");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player")
         {
             director.Play();
-            Destroy(gameObject);
         }
     }
 }
